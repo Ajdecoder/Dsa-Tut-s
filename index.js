0e41b88c6badd63cd4586c11111d8f2d66364c0d
Anuj Kumar Shrivastav <123423315+Ajdecoder@users.noreply.github.com>
2024-02-09 19:36:37 +0530

    commit rec

---
+++ index.js
@@ -0,0 +1,16 @@
+let myObj = {
+    "Name": "Anuj",
+    "Class": "XI",
+    "Season": "34",
+    "isAdmin": "false",
+    "Version": "453.2yurt",
+    "Food": ["Susie", "Halwa", "dosa", "paneer"],
+    "myObj": {
+        "Heading": "Your are King",
+        "Subheading": "You Can Do It",
+        "Roll Code": "Argue34s2",
+        "Food2": ["Susie", "Halwa", "dosa", "paneer", { "so": "true" }]
+    }
+}
+
+console.log(myObj.Food)
